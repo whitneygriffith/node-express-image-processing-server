172fac6d8d45585155c21d04ef4e17f638865481
Whitney Griffith <whitney.griffith16@gmail.com>
2020-11-04 19:12:53 -0800

    Storing the image on disk

--- api/app.js
+++ api/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
+const router = require('./src/router');
 
 const app = express();
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
+app.use('/', router);
+
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 
 app.use('/*', function(request, response) {
   response.sendFile( pathToIndex );
---
+++ api/src/router.js
@@ -0,0 +1,37 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable max-len */
+const {Router} = require('express');
+const multer = require('multer');
+
+const router = Router();
+
+const storage = multer.diskStorage({
+  destination: 'api/uploads/',
+  filename,
+});
+
+function filename(request, file, callback) {
+  callback(null, file.originalname);
+}
+
+function fileFilter(request, file, callback) {
+  if (file.mimetype != 'image/png') {
+    request.fileValidationError = 'Wrong file type';
+    callback(null, false, new Error('Wrong file type'));
+  } else {
+    callback(null, true);
+  }
+}
+
+const upload = multer({
+  fileFilter,
+  storage,
+});
+
+router.post('/upload', upload.single('photo'), (request, response) => {
+  if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
+
+  return response.status(201).json({success: true});
+});
+
+module.exports = router;
