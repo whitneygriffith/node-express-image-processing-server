0c8ac539f290a0d05e07a199ba71d3259cdbbb30
Whitney Griffith <whitney.griffith16@gmail.com>
2020-11-11 11:55:17 -0800

    module 4

--- api/src/router.js
+++ api/src/router.js
@@ -6,6 +6,8 @@ const multer = require('multer');
 const router = Router();
 const path = require('path');
 
+const imageProcessor = require('./imageProcessor');
+
 const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
 const storage = multer.diskStorage({
@@ -31,8 +33,14 @@ const upload = multer({
   storage,
 });
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
   if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
+    
+  try {
+    await imageProcessor(request.file.filename);
+  } catch (error) {
+  
+  }
 
   return response.status(201).json({success: true});
 });
